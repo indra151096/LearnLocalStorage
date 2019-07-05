7580eb229b4e196f1f5c0864ebca5b37207a2c0d
Indra <indra.sumawi@gmail.com>
2019-07-05 09:35:42 +0700

    Initial Commit

---
+++ LearnLocalStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnLocalStorage
+//
+//  Created by Indra Sumawi on 05/07/19.
+//  Copyright © 2019 Indra Sumawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
